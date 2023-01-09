1fe720acc95a468650c54a00bc35e33126fec21f
Davi Luiz <davilucena222@gmail.com>
2023-01-09 02:47:04 +0000

    installing and setting builds

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -3,7 +3,7 @@ import { Button, ButtonProps } from '@ignite-ui/react'
 
 // Tudo que for colocado como configuração global dentro desse bloco será herdado por todas as variações do componente
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
   args: {
     children: 'Button',
