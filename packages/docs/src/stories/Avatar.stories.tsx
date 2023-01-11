0d64e0aa9a5e50646d1e5b81f258fdd06ec04e62
Davi <davilucena222@gmail.com>
2023-01-10 23:54:23 -0300

    editing avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+// Tudo que for colocado como configuração global dentro desse bloco será herdado por todas as variações do componente
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/davilucena222.png',
+    alt: 'Davi Luiz'
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
